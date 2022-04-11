f714051b8113c0f0992d7710df6ab69b85f37d33
Diekkan <3950@holbertonschool.com>
2022-04-11 07:05:12 -0700

    New method delete, updated all method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
-
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        temp = {}
+        if cls != None:
+            for key, value in FileStorage.__objects.items():
+                if cls is type(value):
+                    temp[key] = value
+            return temp
+        else:
+            return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +61,11 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        if obj is None:
+            return
+        for key, value in FileStorage.__objects.items():
+            if str(value) == str(obj):
+                FileStorage.__objects.pop(key)
+                return
