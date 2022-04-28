f04a18e4b9b72736061d66a29dc372758c71947a
Diekkan <merentiel.diego@gmail.com>
2022-04-28 09:05:13 -0700

    0- fixed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,8 +5,8 @@ sudo apt-get install nginx -y
 sudo ufw allow 'Nginx HTTP'
 echo Hello World | sudo tee /var/www/html/index.nginx-debian.html
 sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir /data/web_static/shared/
-echo "<html>\n <head>\n </head>\n  <body>\n   Holberton School\n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/shared/
+printf "<html>\n <head>\n </head>\n  <body>\n   Holberton School\n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/server_name _;/a\location \/hbnb_static {\n alias \/data\/web_static\/current\/;\n}' /etc/nginx/sites-available/default
