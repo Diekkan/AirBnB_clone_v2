c080586e4954ae7d249dd7751c716583f2efa08f
Diekkan <merentiel.diego@gmail.com>
2022-04-28 09:02:16 -0700

    1-pack fixed

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Fabric script that generates a .tgz archive from the contents of the web_static """
+""" Fabric script that generates a .tgz archive
+from the contents of the web_static """
 
 from fabric.api import local
 import datetime
@@ -9,7 +10,7 @@ def do_pack():
     """ makes tgz compressed file """
     now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     local("mkdir -p versions")
-    file = local("tar -czvf versions/web_static_{}\.tgz web_static".format(now))
+    file = local("tar -czvf versions/web_static_{}.tgz web_static".format(now))
     if file:
         return("versions/web_static_{}".format(now))
     else:
