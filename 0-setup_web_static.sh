0728efe1bc3ec334b0f3377bcb2029591e5fc6a0
Diekkan <merentiel.diego@gmail.com>
2022-04-28 15:50:58 +0000

    a Bash script that sets up your web servers for the deployment of web_static.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,12 +3,11 @@
 sudo apt-get update
 sudo apt-get install nginx -y
 sudo ufw allow 'Nginx HTTP'
-sudo ufw enable
 echo Hello World | sudo tee /var/www/html/index.nginx-debian.html
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir /data/web_static/shared/
-echo HelloWorld | sudo tee /data/web_static/releases/test/index.html
+echo "<html>\n <head>\n </head>\n  <body>\n   Holberton School\n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/server_name _;/a\location \/hbnb_static\/ {\n alias \/data\/web_static\/current\/;\n}' /etc/nginx/sites-available/default
+sudo sed -i '/server_name _;/a\location \/hbnb_static {\n alias \/data\/web_static\/current\/;\n}' /etc/nginx/sites-available/default
 sudo service nginx restart
